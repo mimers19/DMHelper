683056197fb098e8f87bd4fab5a66ecdc12cd16c
Dominik Gołembowski <mimers19@gmail.com>
2023-07-10 15:59:04 +0200

    button

---
+++ DMHelper/Core/Window.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DMHelper.Core
+{
+    internal class Window : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));          //Refreshes window when something changes
+        }
+    }
+}

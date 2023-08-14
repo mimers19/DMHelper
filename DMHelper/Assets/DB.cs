498218e786e1225485391b996573e4e6890d7a78
mimers19 <mimers19@gmail.com>
2023-08-14 07:48:31 +0200

    db proba

---
+++ DMHelper/Assets/DB.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.Sqlite;
+
+namespace DMHelper.Assets
+{
+    internal static class DB
+    {
+        private static string connString = "Data Source=C:\\Users\\dell\\OneDrive\\Dokumenty\\GitHub\\DMHelper\\DMHelper\\DataBase\\DMHelperDB.db";
+        static SqliteConnection connection;
+
+        public static void Connect()
+        {
+            connection = new SqliteConnection(connString);
+            Console.WriteLine("Success");
+        }
+
+        public static void Disconnect()
+        {
+
+        }
+    }
+}
